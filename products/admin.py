d2f1d492c5eab0eccb0447986bbfa45ed4d9533e
Kamaljeet <kamaljeetsahoo@gmail.com>
2021-10-31 23:35:12 +0530

    custom profile added

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ProductImage, Product, Brand
+from .models import ProductImage, Product, Brand, Profile
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Brand)
+admin.site.register(Profile)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.db.models.deletion import SET_NULL
 from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = "profile_images", null=True, blank=True)
+    coins = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
 class Brand(models.Model):
     brand_name = models.CharField(max_length=100)
     logo = models.ImageField(upload_to="brand_images", null=True)
